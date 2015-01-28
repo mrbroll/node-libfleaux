baa0c934d5ecf832b6a9e2942f03749fcaf763af
Michael Broll <michael.r.broll@gmail.com>
2015-01-27 21:04:14 -0600

    updated linux dep

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var fleaux = null;
-
-if (process.platform === 'linux') { //linux
-    fleaux = require('./lib/linux/fleaux');
-} else if (process.platform === 'darwin') { //macintosh
-    fleaux = require('./lib/mac/fleaux');
-} else if (process.platform === 'win32') { //windows
-    fleaux = require('./lib/win/fleaux');
-} else {
-    fleaux = require('./build/Release/fleaux');
-}
+var fleaux = require('./build/Release/fleaux');
 
 module.exports = fleaux;
